a6d60d5ec146acc5c2c3a8fa3e34a8a726325355
dev <jayantimahapatra08@gmail.com>
2018-02-21 01:12:13 +0530

    add 1st fiel

---
+++ Mypack/Classstring.java
@@ -0,0 +1,53 @@
+package Mypack;
+
+public class Classstring {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x="iopi";
+		int y=x.length(); 
+		;
+		int flag=0;
+		
+		for(int i=1;i<=y-1;i++)
+		{	char ip=x.charAt(i);
+		
+			for(int j=0;j<=i-1;j++)
+			{
+			
+			
+				char  pa=x.charAt(j);
+				
+			
+				if(ip==pa){
+					
+					flag=flag+1;
+					if(flag==1)
+					{
+
+						System.out.print(pa);	
+						
+					}
+					
+					
+					
+				
+					
+				}
+				
+				
+				
+				
+			}
+			
+		}
+		
+
+		}
+
+	}
+
+
