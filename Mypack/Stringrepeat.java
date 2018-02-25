eec201ca5c7fa03b76cebade0fc6c2e0a2e1d069
ipsita <pattnaik.ipsita13@gmail.com>
2018-02-26 03:02:02 +0530

    string

---
+++ Mypack/Stringrepeat.java
@@ -0,0 +1,19 @@
+package Mypack;
+
+import java.util.Scanner;
+
+public class Stringrepeat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println("enter a string");
+		Scanner sc=new Scanner(System.in);
+		String x=sc.nextLine();
+		
+		int length=x.length();
+	}
+
+}
