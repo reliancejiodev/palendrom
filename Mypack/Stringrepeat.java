73a5a9a8103a9cf2450147b54d46d6d45a2de92e
ipsita <pattnaik.ipsita13@gmail.com>
2018-02-26 23:01:36 +0530

    add string repeat

--- Mypack/Stringrepeat.java
+++ Mypack/Stringrepeat.java
@@ -12,8 +12,29 @@ public class Stringrepeat {
 		System.out.println("enter a string");
 		Scanner sc=new Scanner(System.in);
 		String x=sc.nextLine();
+		int flag=0;
 		
 		int length=x.length();
+		for(int i=1;i<=length-1;i++)
+		{
+			char in=x.charAt(i);
+			for(int j=0;j<i;j++)
+				
+					{
+				char kl=x.charAt(j);
+				if(in==kl)
+				{
+					flag=flag+1;
+					if(flag==1)
+					{
+					System.out.print(in);
+					
+				}
+					
+				}
+				
+					}
+		}
 	}
 
 }
